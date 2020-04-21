2d70aa675cf2e5d19b9555779010e752dd70af0b
Lauren Kye <lkye19@gmail.com>
2020-04-20 21:36:17 -0500

    Add to Homework file

---
+++ SavvyHomework_Week_2.js
@@ -0,0 +1,21 @@
+// Name of Pizza Place.
+const pizzaPlace = "Pizza Palace!!!";
+// Number of Toppings.
+let numberOfToppings = 12;
+
+// literal template
+console.log("Welcome!!! One and all to the fabulous" + pizzaPlace + ". Where you can put " + numberOfToppings + " Toppings on ONE PIZZA!!! ");
+
+// if statment
+if (numberOfToppings >= 12) {
+  console.log("A whole lot of pizza.");
+} else {
+  console.log("Quality, not quantity.");
+}
+
+//Loop Bonus
+for (let i = 1; i <= 12; i++) {
+  console.log(i);
+}
+
+
